82cb1917389965ae283c85a427ad4d8abf621da4
Chien-Ming Huang <cmhuang@dyn-72-33-131-184.uwnet.wisc.edu>
2011-09-23 21:23:39 -0500

    Integrated timeline into google app.

--- main.py
+++ main.py
@@ -16,18 +16,41 @@
 #
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
-
+from google.appengine.ext.webapp import template
+import os
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('<html><body>Hello world!</body></html>')
+        #self.response.out.write('<html><body>Hello world!</body></html>')
 
+	guestbook_name=self.request.get('guestbook_name')
+        #greetings_query = Greeting.all().ancestor(
+        #    guestbook_key(guestbook_name)).order('-date')
+        greetings = 'lala'#greetings_query.fetch(10)
+	url = 'this is url'
+        '''if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+	'''
+        template_values = {
+            'greetings': greetings,
+            'url': url,
+	    'data': 'examples/jfk/jfk.xml',
+            #'url_linktext': url_linktext,
+        }
+	
+	path = os.path.join(os.path.dirname(__file__), 'index.html')
+	self.response.out.write(template.render(path, template_values))
 
 def main():
     application = webapp.WSGIApplication([
        ('/', MainHandler),
        ('/canvas', MainHandler),
        ('/canvas/', MainHandler)
+       #('/examples/jfk/', MainHandler)
        ],
                                          debug=True)
     util.run_wsgi_app(application)
